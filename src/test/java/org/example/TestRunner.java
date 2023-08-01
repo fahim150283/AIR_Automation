5b37084479627d5a15092b6e1af98925e8ce8a7b
AHChowdhuryFahim <135217832+AHChowdhuryFahim@users.noreply.github.com>
2023-08-01 16:47:17 +0600

    final

--- src/test/java/org/example/TestRunner.java
+++ src/test/java/org/example/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         plugin = {"pretty","html:target/cucumber-reports-html/cucumber.html",
                 "json:target/cucumber-reports/cucumber.json"},
-        features = {"G:/Abul Hossain Chowdhury/Projects/Ultimate_Cucumber/src/test/resources/Features"},
+        features = {"G:/Abul Hossain Chowdhury/Projects/Ultimate_Cucumber_learning/src/test/resources/Features"},
         glue = {"org.example.StepDefinitions"},
         tags = "@regression" //it runs all the scenarios of the feature file that contains @regression
 //        tags = "@google"  //it runs all the scenarios of the feature file that contains @google
