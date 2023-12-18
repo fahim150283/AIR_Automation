db4ccb8012284d3177323aacbc986ef258c1b934
Abul Hossain Chowdhury <fahim150283@gmail.com>
2023-12-18 11:29:15 +0600

    Apps modifications

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,22 +1,5 @@
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import org.example.Page_Options;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Objects;
-import java.util.Random;
 
 public class test extends Page_Options {
     String arr[] = new String[2];
