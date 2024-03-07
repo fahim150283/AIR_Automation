8937040368ac7aef907bdffc4af0b48260ffe751
Abul Hossain Chowdhury <fahim150283@gmail.com>
2024-03-07 17:30:11 +0600

    contacts assertion is pending only

--- src/test/java/org/example/StepDefinitions/Contacts.java
+++ src/test/java/org/example/StepDefinitions/Contacts.java
@@ -6,9 +6,12 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.example.Page_Options;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+
+import java.util.List;
 
 public class Contacts extends Page_Options {
 
@@ -37,24 +40,36 @@ public class Contacts extends Page_Options {
     }
 
     @And("verify if the the employee is searched or not")
-    public void verifyIfTheTheEmployeeIsSearchedOrNot() {
+    public void verifyIfTheTheEmployeeIsSearchedOrNot() throws InterruptedException {
+        String s = null;
+        Thread.sleep(100);
 
-        String s="";
+        // Find the parent element containing all the employee details
+        WebElement wrapper = driver.findElement(By.id("wrapper"));
 
-        // view the contact details
-        WebElement table = driver.findElement(By.id("wrapper"));
-        java.util.List<WebElement> rows = table.findElements(By.xpath(".//div"));
-        // Iterate through rows
-        for (WebElement row : rows) {
-            // Check if the row is displayed
-            if (!row.getAttribute("style").contains("display: none;")) {
-                // Find and click the "Add App Permissions" button for the visible row
-                WebElement view_Button = row.findElement(By.xpath("/div/div/div/span/p[1]"));
-                s = view_Button.getText();
+        // Find all the visible boxes
+        List<WebElement> boxes = wrapper.findElements(By.xpath(".//div[@class='col-md-4 box' and not(contains(@style, 'display: none;'))]"));
+
+        // Iterate through each visible box
+        for (WebElement box : boxes) {
+            // Find the <p> element with class 'name' inside each box
+            WebElement nameElement = null;
+            try {
+                nameElement = box.findElement(By.xpath(".//p[@class='name']"));
+                // Get the text of the <p> element
+                String name = nameElement.getText();
+                // Print or store the name as needed
+                System.out.println("Found contact name: " + name);
+                // You can break out of the loop if needed, depending on your use case
+                s = name;
+            } catch (NoSuchElementException e) {
+                // If the name element is not found in the current box, continue to the next box
+                continue;
             }
         }
-        xpath = "//*[@id=\"wrapper\"]/div[1]/div/div/span/p[1]";
-        System.out.println(s+" is the found contact name");
-        Assert.assertEquals(s,ContactsSearchInfo);
+
+        // Assert if the found name matches the expected name
+        Assert.assertEquals(s, ContactsSearchInfo);
     }
+
 }
